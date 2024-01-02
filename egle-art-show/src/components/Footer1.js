234562442547fa984e9cfcbe20f91980f69af5c2
Auksea <66auksyte@gmail.com>
2024-01-02 12:33:58 +0100

    .

--- egle-art-show/src/components/Footer1.js
+++ egle-art-show/src/components/Footer1.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import './Footer1.css'; // Import your CSS file
+import './Footer1.css';
 
 const Footer1 = () => {
   return (
