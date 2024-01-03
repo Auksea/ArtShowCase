87650290b57dc88e6e853cd9b2517e8480cf4348
Auksea <66auksyte@gmail.com>
2024-01-03 09:45:56 +0100

    Styled about author page

--- egle-art-show/src/components/About.js
+++ egle-art-show/src/components/About.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './About.css'; // Import the CSS file
 
 const About = () => {
   return (
-    <div>
-      <p>Content</p>
+    <div className="background">
+      <div className="text-container">
+        <h1>Apie Autorių</h1>
+        <p>Čia tavo tekstas kurį duosi apie save</p>
+        <p>Pvz, tavo idėjos</p>
+        <p>Ir kažkokie asmeniniai išgyvenimai</p>
+        <p>Ar apie kūrybą</p>
+        <p className="citation">
+          "Čia galima įdėti kokią nors citatą"
+        </p>
+      </div>
     </div>
   );
 };
